0b4aed46ec84a66d1529722d09f7e7cd4ed22509
xrotwang <xrotwang@googlemail.com>
2018-04-13 16:01:08 +0200

    renamed

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+import sys
+import json
+
+
+PY2 = sys.version_info.major == 2
+with open('metadata.json', **({} if PY2 else {'encoding': 'utf-8'})) as fp:
+    metadata = json.load(fp)
+
+
+setup(
+    name='lexibank_kraftchadic',
+    description=metadata['title'],
+    license=metadata.get('license', ''),
+    url=metadata.get('url', ''),
+    py_modules=['lexibank_kraftchadic'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        'lexibank.dataset': [
+            'kraftchadic=lexibank_kraftchadic:Dataset',
+        ]
+    },
+    install_requires=[
+        'pylexibank',
+    ]
+)
