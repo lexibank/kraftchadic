09605838bad5d4f8db4d67ec50712c538e667d9a
Christoph Rzymski <christoph@functor-argument.net>
2019-07-02 12:28:55 +0200

    Code cleanup, Glottolog 4.0., moved orthography profile.

--- README.md
+++ README.md
@@ -11,15 +11,22 @@ Conceptlists in Concepticon:
 
 Any dataset specific notes on lexibank decisions/mapping choices etc go in here.
 
+
 ## Statistics
+
 
 [![Build Status](https://travis-ci.org/lexibank/kraftchadic.svg?branch=master)](https://travis-ci.org/lexibank/kraftchadic)
 ![Glottolog: 99%](https://img.shields.io/badge/Glottolog-99%25-green.svg "Glottolog: 99%")
 ![Concepticon: 100%](https://img.shields.io/badge/Concepticon-100%25-brightgreen.svg "Concepticon: 100%")
 ![Source: 100%](https://img.shields.io/badge/Source-100%25-brightgreen.svg "Source: 100%")
+![BIPA: 52%](https://img.shields.io/badge/BIPA-52%25-red.svg "BIPA: 52%")
+![CLTS SoundClass: 52%](https://img.shields.io/badge/CLTS%20SoundClass-52%25-red.svg "CLTS SoundClass: 52%")
 
 - **Varieties:** 68
 - **Concepts:** 428
 - **Lexemes:** 29,493
 - **Synonymy:** 1.04
-
+- **Invalid lexemes:** 0
+- **Tokens:** 167,711
+- **Segments:** 518 (248 BIPA errors, 248 CTLS sound class errors, 271 CLTS modified)
+- **Inventory size (avg):** 84.65
--- lexibank_kraftchadic.py
+++ lexibank_kraftchadic.py
@@ -1,21 +1,17 @@
-# coding=utf-8
-from __future__ import unicode_literals, print_function
 from itertools import groupby
 
 import lingpy
-from pycldf.sources import Source
-from clldutils.text import strip_chars
-from clldutils.path import Path
 from clldutils.misc import slug
-
-from pylexibank.dataset import Metadata
+from clldutils.path import Path
+from clldutils.text import strip_chars
+from pycldf.sources import Source
 from pylexibank.providers import qlc
 
 
 class Dataset(qlc.QLC):
     dir = Path(__file__).parent
-    id = 'kraftchadic'
-    DSETS = ['kraft1981.csv']
+    id = "kraftchadic"
+    DSETS = ["kraft1981.csv"]
 
     def cmd_install(self, **kw):
         # column "counterpart_doculect" gives us the proper names of the doculects
@@ -24,46 +20,54 @@ class Dataset(qlc.QLC):
         # get the language identifiers stored in wl._meta['doculect'] parsed from input
         # file
         lids = {}
-        for line in wl._meta['doculect']:
-            rest = line.split(', ')
+        for line in wl._meta["doculect"]:
+            rest = line.split(", ")
             name = rest.pop(0)
             lids[name] = rest.pop(0)
 
         concepts = {
-            strip_chars('()?', c.english).upper().replace("'", ' '): c.concepticon_id
-            for c in self.conceptlist.concepts.values()}
+            strip_chars("()?", c.english).upper().replace("'", " "): c.concepticon_id
+            for c in self.conceptlist.concepts.values()
+        }
 
-        src = Source.from_bibtex("""
+        src = Source.from_bibtex(
+            """
 @book{Kraft1981,
     author={Kraft, Charles H.},
     title={Chadic Wordlists.},
     year={1981},
     address={Berlin},
     publisher={D. Reimer}
-}""")
+}"""
+        )
 
-        concept_map = {
-            'MAT TABARME': 'MAT TABARMA'
-        }
+        concept_map = {"MAT TABARME": "MAT TABARMA"}
 
         def grouped_rows(wl):
             rows = [
-                (wl[k, 'counterpart_doculect'], wl[k, 'concept'], wl[k, 'counterpart'], wl[k, 'qlcid'])
-                for k in wl]
+                (
+                    wl[k, "counterpart_doculect"],
+                    wl[k, "concept"],
+                    wl[k, "counterpart"],
+                    wl[k, "qlcid"],
+                )
+                for k in wl
+            ]
             return groupby(sorted(rows), key=lambda r: (r[0], r[1]))
 
         with self.cldf as ds:
             ds.add_sources(src)
             for (language, concept), rows in grouped_rows(wl):
                 iso = lids[language]
-                concept = concept.replace('_', ' ')
+                concept = concept.replace("_", " ")
                 cid = concepts[concept_map.get(concept, concept)]
 
                 ds.add_language(
                     ID=slug(language),
                     Name=language.capitalize(),
                     ISO639P3code=lids[language],
-                    Glottocode=self.glottolog.glottocode_by_iso.get(iso, ''))
+                    Glottocode=self.glottolog.glottocode_by_iso.get(iso, ""),
+                )
                 ds.add_concept(ID=cid, Name=concept, Concepticon_ID=cid)
 
                 for i, (l, c, form, id_) in enumerate(rows):
@@ -72,4 +76,5 @@ class Dataset(qlc.QLC):
                         Parameter_ID=cid,
                         Value=form,
                         Source=[src.id],
-                        Local_ID=id_)
+                        Local_ID=id_,
+                    )
